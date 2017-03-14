68599434cda8b06dc48aa8517c3def4345b41195
Brunner <brunnel6@troulwn0123.emea.roche.com>
2017-03-14 10:53:33 +0100

    show game master version script startup

--- bin/get-latest-game-master.js
+++ bin/get-latest-game-master.js
@@ -1,16 +1,38 @@
-var https = require('https');
-var fs = require('fs');
-var path = require('path');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
-var latestGameMasterPath = 'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-game-master/master/versions/latest/GAME_MASTER.json';
-var dir = path.join(__dirname, '../src/data');
-var file = path.join(dir, 'GAME_MASTER.json');
-if (!fs.existsSync(dir)) {
-	fs.mkdirSync(dir);
+const latestGameMasterPath = 'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-game-master/master/versions/latest/GAME_MASTER.json';
+const latestVersionPath = 'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-game-master/master/versions/latest-version.txt';
+
+const dir = path.join(__dirname, '../src/data');
+const file = path.join(dir, 'GAME_MASTER.json');
+const latestVersionFile = path.join(dir, '.json');
+
+
+function writeVersionToGameMaster(data) {
+    fs.writeFileSync(file, JSON.stringify(gameMaster));
 }
 
 
-var file = fs.createWriteStream(file);
-var request = https.get(latestGameMasterPath, function (response) {
-	response.pipe(file);
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
+
+var fileStream = fs.createWriteStream(file);
+https.get(latestGameMasterPath, (response) => {
+    let gameMasterData = '';
+    response.on('data', (d) => {
+        gameMasterData += d.toString('utf8');
+    });
+    response.on('end', () => {
+        let gameMaster = JSON.parse(gameMasterData);
+        https.get(latestVersionPath, (response) => {
+            response.on('data', (d) => {
+                gameMaster.version = d.toString('utf8');
+                fs.writeFileSync(file, JSON.stringify(gameMaster));
+            })
+        });
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,22 @@ import * as path from 'path';
 import * as chalk from 'chalk';
 
 import { GameMaster } from './core/game_master';
-// import { PokemonParser } from 'components/pokemon';
+import { PokemonParser } from './components/pokemon/pokemon.parser';
 
 const gameMaster = require('./data/GAME_MASTER.json');
+const packageJson = require('../package.json');
 
 const OUTPUT_PATH = 'output';
 const POKEMON_PATH = path.join(OUTPUT_PATH, 'pokemon.json')
 
-// fs.writeFile(POKEMON_PATH, JSON.stringify(new PokemonParser(gameMaster).parse()), function (err) {
-//     if (err) {
-//         console.log(chalk.red('×') + ' Failed at Parsing Pokemon')
-//         return console.log(err);
-//     }
+console.log(`${chalk.blue('i')} ${packageJson.name} ${chalk.cyan(packageJson.version)} `);
+console.log(`${chalk.blue('i')} Using GAME_MASTER version ${chalk.cyan(gameMaster.version)}`);
 
-//     console.log(chalk.green('✓') + ' Pokemon written to ' + POKEMON_PATH);
-// });
+fs.writeFile(POKEMON_PATH, JSON.stringify(new PokemonParser(gameMaster).parse()), function (err) {
+    if (err) {
+        console.log(`${chalk.red('×')} Failed at Parsing Pokemon`);
+        return console.log(err);
+    }
+
+    console.log(`${chalk.green('✓')} Pokemon written to ${chalk.cyan(POKEMON_PATH)}`);
+});
