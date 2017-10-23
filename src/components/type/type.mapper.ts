de39f05e56364a34f00eb4ead9fe1bdbf0163e08
Valter Pires <valter.pires@gmail.com>
2017-10-23 22:16:07 +0400

    Add pokemon types - Refactoring

--- src/components/type/type.mapper.ts
+++ src/components/type/type.mapper.ts
@@ -1,22 +1,20 @@
 import { Type } from './type.model';
 import { ItemTemplate } from '@core/game_master/gameMaster';
 import { Util } from '@util';
+import APP_SETTINGS from '@settings/app';
 
 export class TypeMapper {
-    public static Map(rawType: ItemTemplate, typeIds: Map<number, string>): Type {
+    public static Map(rawType: ItemTemplate): Type {
         let type: Type = new Type();
 
-        type.id = rawType.templateId
+        type.id = rawType.templateId;
         type.name = Util.SnakeCase2HumanReadable(rawType.templateId.replace('POKEMON_TYPE_', ''));
 
-        type.damage = [];
-        for (let i = 0; i < rawType.typeEffective.attackScalar.length; i++) {
-            type.damage.push({
-                id: typeIds.get(i),
-                attackScalar: rawType.typeEffective.attackScalar[i]
-            });
-        }
+        type.damage = APP_SETTINGS.POKEMON_TYPES.map((pokemonType, index) => { return {
+            id: pokemonType.id,
+            attackScalar: rawType.typeEffective.attackScalar[pokemonType.attackScalarIndex]
+        }});
 
-        return type
+        return type;
     }
 }
