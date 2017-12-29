3c9559f2948b75061ea401678add688555430a82
Livio Brunner <livio.brunner.lb1@gmail.com>
2017-12-29 10:16:31 -0800

    Refactor pokemon model

---
+++ src/outcome/pokemon/index.ts
@@ -0,0 +1,6 @@
+export * from './pokemon';
+export * from './pokemonCamera';
+export * from './pokemonEncounter';
+export * from './pokemonEvolutionTree';
+export * from './pokemonGender';
+export * from './pokemonStats';
