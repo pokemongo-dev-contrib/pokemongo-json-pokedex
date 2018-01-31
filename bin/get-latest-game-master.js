bfb0937b131f3745ee5ab14ec13e58d0290f6b57
Livio <livio.brunner.lb1@gmail.com>
2018-01-31 11:20:52 +0100

    Use pokemongo-game-master library
    Resolves #46

--- bin/get-latest-game-master.js
+++ bin/get-latest-game-master.js
@@ -1,13 +1,13 @@
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
+const pokemongoGameMaster = require('pokemongo-game-master');
 
 const latestGameMasterPath = 'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-game-master/master/versions/latest/GAME_MASTER.json';
 const latestVersionPath = 'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-game-master/master/versions/latest-version.txt';
 
 const dir = path.join(__dirname, '../src/data');
 const file = path.join(dir, 'GAME_MASTER.json');
-const latestVersionFile = path.join(dir, '.json');
 
 if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
@@ -15,18 +15,9 @@ if (!fs.existsSync(dir)) {
 
 
 var fileStream = fs.createWriteStream(file);
-https.get(latestGameMasterPath, (response) => {
-    let gameMasterData = '';
-    response.on('data', (d) => {
-        gameMasterData += d.toString('utf8');
-    });
-    response.on('end', () => {
-        let gameMaster = JSON.parse(gameMasterData);
-        https.get(latestVersionPath, (response) => {
-            response.on('data', (d) => {
-                gameMaster.version = d.toString('utf8');
-                fs.writeFileSync(file, JSON.stringify(gameMaster));
-            });
-        });
+pokemongoGameMaster.getVersion('latest', 'json').then(gameMaster => {
+    pokemongoGameMaster.getLatestVersionName().then(version => {
+        gameMaster.version = version;
+        fs.writeFileSync(file, JSON.stringify(gameMaster, null, 4));
     });
 });
