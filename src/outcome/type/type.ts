ad91f6bc511712370f0cf84688a9982f7508e374
Livio <livio.brunner.lb1@gmail.com>
2018-01-31 09:16:01 +0100

    Add comments for type model

--- src/outcome/type/type.ts
+++ src/outcome/type/type.ts
@@ -2,8 +2,21 @@ import { Identifyable } from '@core/identifyable';
 import { TypeDamage } from './typeDamage';
 
 export class Type implements Identifyable {
+    /**
+     * The name of the Type
+     * @example
+     * Bug
+     */
     public name: string;
+    /**
+     * The id of the type
+     * @example
+     * POKEMON_TYPE_BUG
+     */
     public id: string;
+    /**
+     * The array of attack scalars
+     */
     public damage: TypeDamage[];
 }
 
