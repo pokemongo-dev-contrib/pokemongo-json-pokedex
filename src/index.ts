c314eefb9d1cb0cf53cdb8542446a77700fdb67b
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-07-09 11:40:59 +0200

    Fix npm package error

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import 'module-alias/register';
+export { AvatarCustomizationPipeline } from './processing/avatarCustomization';
+export { MovePipeline } from './processing/move';
+export { PokemonPipeline } from './processing/pokemon';
+export { TypePipeline } from './processing/type';
+export { Pipeline } from './core';
