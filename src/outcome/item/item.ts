a666d75ce4e9f031452ac74f5df94a15d65731f1
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-07-25 15:37:29 +0200

    Add item pipeline

---
+++ src/outcome/item/item.ts
@@ -0,0 +1,20 @@
+import { Identifyable } from '@core/identifyable';
+export class Item {
+    /**
+     * The identifier of the item
+     */
+    public id: string;
+    /**
+     * The human readable name of the item
+     */
+    public name: string;
+    /**
+     * The category of the item
+     * e.g. ITEM_CATEGORY_FOOD
+     */
+    public category: Identifyable;
+    /**
+     * The level on which level this item gets dropped
+     */
+    public dropTrainerLevel: number;
+}
