b15d27a78a1d6ae2cc474ef0ac5cd15cc354cb74
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-07-26 18:11:05 +0200

    Fix not unique Mew moves
    Fixes issue #6 on Github
    Add test to verify each move is unique

--- src/processing/pokemon/components/cinematicMoves.ts
+++ src/processing/pokemon/components/cinematicMoves.ts
@@ -1,14 +1,20 @@
-import { Component, IComponent } from '@core/pipeline';
-import { ItemTemplate } from '@income';
-import { Pokemon } from '@outcome/pokemon';
-import { Util } from '@util';
+import { Component, IComponent } from '@core/pipeline/index';
+import { ItemTemplate } from '@income/index';
+import { Pokemon } from '@outcome/pokemon/index';
+import { Util } from '@util/index';
+import * as _ from 'lodash';
 
 @Component({
   pipeline: 'pokemon'
 })
 export class CinematicMoves implements IComponent {
   Process(pokemon: Pokemon, rawPokemon: ItemTemplate): Pokemon {
-    pokemon.cinematicMoves = rawPokemon.pokemonSettings.cinematicMoves.map(Util.SnakeCase2Identifyable);
+    const cinematicMoves = _
+      .chain(rawPokemon.pokemonSettings.cinematicMoves)
+      .uniqBy('id')
+      .map(Util.SnakeCase2Identifyable)
+      .value();
+    pokemon.cinematicMoves = cinematicMoves;
     return pokemon;
   }
 }
