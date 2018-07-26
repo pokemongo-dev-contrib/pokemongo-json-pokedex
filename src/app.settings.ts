58982d871c94d1ae9ee856367c328abf3f33b56b
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-07-26 19:11:10 +0200

    Remove _NORMAL entries
    Fixes issue #8

--- src/app.settings.ts
+++ src/app.settings.ts
@@ -1,9 +1,4 @@
 const APP_SETTINGS = {
-    OUTPUT_PATH: 'output',
-    POKEMON_FILE: 'pokemon.json',
-    TYPE_FILE: 'type.json',
-    MOVE_FILE: 'move.json',
-    AVATAR_CUSTOMIZATION_FILE: 'avatar-customization.json',
     MAX_CP_MULTIPLIER: 0.79030001,
     MAX_INDIVIDUAL_STAMINA: 15,
     MAX_INDIVIDUAL_ATTACK: 15,
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { AvatarCustomizationPipeline } from './processing/avatarCustomization/in
 export { MovePipeline } from './processing/move/index';
 export { PokemonPipeline } from './processing/pokemon/index';
 export { TypePipeline } from './processing/type/index';
-export { ItemPipeline } from './processing/item/index';
+export { ItemPipeline } from './processing/item';
 export { Pipeline } from './core/index';
